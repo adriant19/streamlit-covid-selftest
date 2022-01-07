ccb7fab3cc3d08ee4a16c23f56d788d8042ad04e
adrian.tan <adrian.tlj@gmail.com>
2022-01-08 01:33:51 +0800

    with gitignore

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
-import streamlit
+import streamlit as st
+from google.oauth2 import service_account
+
+# create connection
+
+credentials = service_account.Credentials.from_service_account_file(
+    st.secrets["gcp_service_account"],
+    scopes=["https://www.googleapis.com/auth/spreadsheets"]
+)
+
+conn = connect(credentials=credentials)
